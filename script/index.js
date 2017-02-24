761d889f989146ea6e9f2f3249ab8ea104e59877
Modern Coding <michel.boretti@gmx.de>
2017-02-24 15:07:15 +0700

    Application using Javascript

---
+++ script/index.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+
+	var peopleMenu = document.getElementById('people-menu');
+	var peoples = document.getElementsByClassName('people');
+
+	peopleMenu.addEventListener('mouseover', function(){
+
+		for (var i = peoples.length - 1; i >= 0; i--) {
+			peoples[i].classList.remove('hidden');
+		}
+
+	});
+
+}
--- script/studies.js
+++ script/studies.js
@@ -2,13 +2,33 @@ window.onload = function () {
 
 	var tds = document.getElementsByTagName('td');
 	var trs = document.getElementsByTagName('tr');
+	var myTd = document.getElementById('my-td');
 
 	for (var i=0; i<tds.length; i++){
 		console.log(tds[i].innerText);
 
 		tds[i].addEventListener('click', function(event){
 			event.preventDefault();
-			alert(this.innerText);
+
+			var highlighted = document.getElementsByClassName('highlighted');
+
+			for (var i = highlighted.length - 1; i >= 0; i--) {
+				highlighted[i].classList.remove('highlighted');
+			}
+
+			this.classList.add('highlighted');
+		});
+
+
+		tds[i].addEventListener('mouseover', function(event){
+			event.preventDefault();
+			myTd.innerHTML = '<span class="my-td-span">' + this.innerText + '</span>';
+		});
+
+
+		tds[i].addEventListener('mouseout', function(event){
+			event.preventDefault();
+			myTd.innerHTML = '';
 		});
 
 	}
@@ -22,8 +42,8 @@ window.onload = function () {
 	}
 
 
-	trs[0].addEventListener('mouseover', function(event){
+	/*trs[0].addEventListener('mouseover', function(event){
 		event.preventDefault();
 		alert('Micky Mouse has come here !');
-	});
+	});*/
 }
